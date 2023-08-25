20f83181be5ac41d6dd4f7c8f4a54e26e225fa24
whiplade <nmawira13@gmail.com>
2023-08-25 12:14:41 +0300

    review.py

--- lib/Review.py
+++ lib/Review.py
@@ -1,9 +1,12 @@
 class Review:
+
+    all_reviews = []
     # Initialize a Review object with a customer, restaurant and rating
     def __init__(self, customer, restaurant, rating:int):
         self.customer = customer
         self.restaurant = restaurant
         self.rating = rating
+        self.all_reviews.append(self)
 
     def get_rating(self):
         return self.rating
@@ -11,5 +14,6 @@ class Review:
     def set_rating(self, new_rating):
         self.rating = new_rating
 
-    def all():
-        pass  
+    @classmethod
+    def all(cls):
+        return cls.all_reviews
